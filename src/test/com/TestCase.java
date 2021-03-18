e2994515455e5abb6dde6d5da32ede5710eacf4b
langlitong_v <langlitong_v@didiglobal.com>
2021-03-18 17:22:37 +0800

    调试代码

---
+++ src/test/com/TestCase.java
@@ -0,0 +1,16 @@
+package com;
+
+/**
+ * @Author langlitong_v
+ * @Date 3/18/21 5:03 PM
+ */
+public class TestCase {
+
+    public static void main(String[] args) {
+        String str = "asdfghjiuytrewdfghytre";
+
+        System.out.println(str.substring(3));
+        System.out.println("**&&");
+    }
+
+}
